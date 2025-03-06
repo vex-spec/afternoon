80f9a6d48c8edc9edd5dde7576458ccaa38abe00
VEXALIA <ruoroabigael@gmail.com>
2025-03-06 16:21:31 +0300

    Add files via upload

--- Templates/views.py
+++ Templates/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import render,redirect,get_object_or_404
 from Hospitalapp.models import *
 
 # Create your views here.
@@ -68,3 +71,72 @@ def delete(request,id):
     deletedappointment=Appointment.objects.get(id=id)
     deletedappointment.delete()
     return redirect('/show')
+
+
+def showcontact(request):
+    all = Contact.objects.all()
+    return render(request, 'showcontact.html', {'all': all})
+
+
+
+
+
+def edit(request,id):
+    editinfo = get_object_or_404(Appointment,id=id)
+    if request.method == 'POST':
+        editinfo.name = request.POST.get('name')
+        editinfo.email = request.POST.get('email')
+        editinfo.phone = request.POST.get('phone')
+        editinfo.date = request.POST.get('date')
+        editinfo.department = request.POST.get('department')
+        editinfo.doctor = request.POST.get('doctor')
+        editinfo.message = request.POST.get('message')
+        editinfo.save()
+        return redirect('/show')
+    else :
+        return render(request,'edit.html',{"editinfo":editinfo} )
+
+
+def register(request):
+    """ Show the registration form """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Check the password
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+
+                # Display a message
+                messages.success(request, "Account created successfully")
+                return redirect('/login')
+            except:
+                # Display a message if the above fails
+                messages.error(request, "Username already exist")
+        else:
+            # Display a message saying passwords don't match
+            messages.error(request, "Passwords do not match")
+
+    return render(request, 'register.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        # Check if the user exists
+        if user is not None:
+            # login(request, user)
+            login(request, user)
+            messages.success(request, "You are now logged in!")
+            return redirect('/home')
+        else:
+            messages.error(request, "Invalid login credentials")
+
+    return render(request, 'login.html')
